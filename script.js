85bb9e9801843df2a24e9e54ed5bdf2c5cc934ce
Brayden Bourgeois <128927277+Braydenb1204@users.noreply.github.com>
2024-09-19 08:46:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+
+
+let studentName = "Brayden";
+let studentAge = 20;
+let isEnrolled = true;
+
+console.log(studentName)
+console.log(studentAge)
+console.log(isEnrolled)
+
+
+
+//examples of all 5 data types
+
+//variable
+let greeting = "Hello World!"
+console.log(greeting)
+
+//number
+let balance = 345
+console.log(balance)
+
+//boolean
+let isMale = true
+let isFemale = false
+console.log(isMale)
+console.log(isFemale)
+
+//array
+let systems = ["pc", "playstation", "switch"];
+console.log(`my favourite systems are ${systems}`)
+
+//objects
+const cat = new Object();
+cat .age = 2
+cat .name = "boba";
+console.log(`my cats name is ${cat .name} and he is ${cat .age}`)
+
+
+
+
+//operators
+
+//arithmetic
+console.log(10 * 34); //multiplication
+console.log(34 / 6); //division
+console.log(500 - 340); //subtraction
+console.log(160+340) //addition
+
+//comparison
+let x = 50
+let y = 43
+console.log(x > y);
+console.log(x < y);
+console.log(x != y);
+console.log("50" == x)
+
+//logical
+let a = true
+let b = false
+console.log(a && b); //AND
+console.log(a || b); //OR
+console.log(!b); //NOT
